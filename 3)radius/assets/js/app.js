41cf9057c1cfded4d89007e4caa1a7e36b6a5883
surgik12 <127215052+surgik12@users.noreply.github.com>
2023-04-06 11:27:51 +0300

    3

---
+++ 3)radius/assets/js/app.js
@@ -0,0 +1,10 @@
+    let R = +prompt (`R = см`);
+    console.log (`Радиус = ${R}см`);
+
+    let C = 2 * 3.14 * R;
+    console.log (`Длина окружности = ${C}см`);
+
+    let S = 3.14 * (R * R);
+    console.log (`S = ${S}см2`);
+
+    alert (`Длина окружности = ${C}см, S = ${S}см2`);
