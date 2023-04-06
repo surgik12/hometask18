b0b012e43e8d854941d5d38fc0aef6350f16e8fa
surgik12 <127215052+surgik12@users.noreply.github.com>
2023-04-06 11:36:52 +0300

    4

---
+++ 4)plotnost/assets/js/app.js
@@ -0,0 +1,10 @@
+let m = +prompt (`Масса = кг`);
+console.log(`Масса = ${m} кг`);
+
+let V = +prompt (`Объём = м3`);
+console.log(`Объем ${V} м3`);
+
+let p = m * V;
+console.log(`Плотность ${p} кг м3`);
+
+alert (`Плотность = ${p}кг м3`);
