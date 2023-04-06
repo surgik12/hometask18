b5a67c6a03d8a02ab04496170d2f71ac578ec5d2
surgik12 <127215052+surgik12@users.noreply.github.com>
2023-04-06 11:00:31 +0300

    1

---
+++ 1)Perimetr/assets/js/app.js
@@ -0,0 +1,8 @@
+
+let border = +prompt(`Длинна стороны квадрата`);
+console.log (`Длинна стороны ${border}см`);
+
+let P = border + border + border + border;
+console.log (`Периметр равен ${P}см`);
+
+alert (`Периметр =${P}см`);
